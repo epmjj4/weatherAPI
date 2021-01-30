7189d84eaa4c08e9f6c330e8c1ad34892a8528e6
Nes <epmjj4@hotmail.com>
2021-01-30 09:43:28 -0500

    Updated html, created li tags

--- logic.js
+++ logic.js
@@ -4,31 +4,39 @@ $(document).ready(function () {
     // $('#form-submit').submit(function (event) {
     //     performSearch(event);
     // });
-
-    $("#search-button").con('click', function () {
+    //on click function to grab user input city
+    $("#search-button").on('click', function () {
         // grab the city and put in the variable
         var cityInput = $('#search-value').val();
 
         $('#search-value').val('');
-        performSearch();
+        performSearch(cityInput);
 
     })
+    //created an li tag to 
+    function makeList(val) {
+        var liEl = $('<li>').addClass('list-group-item list-group-item-action').text(val);
+        $('.city-history').append(liEl);
+
+    }
+    $('.city-history').on('click', 'li', function () {
+        performSearch($(this).text());
+    })
+
+
 
     function performSearch(cityInput) {
-
-
+        var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + "&appid=df50ae2fd0ab218ec984115a69f5ef16"
         $.ajax({
-            url: 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + "&appid=df50ae2fd0ab218ec984115a69f5ef16"
-                type: 'GET',
-            data: {
-                q: $("#city").val(),
-                units: 'imperial'
-                
-            }
-        });
-        request.done(function (response) {
-            formatSearch(response)
-        });
+            url: queryURL,
+            type: 'GET',
+            dataType: "json"
+        }).then(function (res) {
+
+        })
+        // request.done(function (response) {
+        //     formatSearch(response)
+        // });
     }
 
     function formatSearch(jsonObject) {
